7e4d402869852b7b788a8be01bb7752eac1943d4
tlwman <miguel_ft@yahoo.com.ar>
2013-04-30 19:47:28 -0300

    sin comentarios

--- practicas_ayed/src/p5e2/TestArbolGenerico.java
+++ practicas_ayed/src/p5e2/TestArbolGenerico.java
@@ -7,7 +7,7 @@ public class TestArbolGenerico {
 	public static void main(String[] args){
 		
 		ArbolGeneral<String> fs = new ArbolGeneral<String>();
-		/*
+		
 		fs.agregarHijo( new ArbolGeneral<String>("usr"));
 		fs.agregarHijo( new ArbolGeneral<String>("bin"));
 		fs.agregarHijo( new ArbolGeneral<String>("home"));
@@ -17,12 +17,12 @@ public class TestArbolGenerico {
 		
 		fs.getHijos().elemento(2).agregarHijo( new ArbolGeneral<String>("usuario"));
 		fs.getHijos().elemento(2).getHijos().elemento(0).agregarHijo( new ArbolGeneral<String>("Documentos"));
-		*/
-		System.out.println("Altura: "+fs.altura());/*
+		
+		System.out.println("Altura: "+fs.altura());
 		System.out.println("Nivel de 'root': "+fs.nivel("root"));
 		System.out.println("Nivel de 'bin': "+fs.nivel("bin"));
 		System.out.println("Nivel de 'usuario': "+fs.nivel("usuario"));
-		System.out.println("Nivel de 'foo': "+fs.nivel("foo"));*/
+		System.out.println("Nivel de 'foo': "+fs.nivel("foo"));
 		System.out.println("Ancho del arbol: "+fs.ancho());
 	}
 }
