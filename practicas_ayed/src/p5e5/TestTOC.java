f2da006812fe50da9c2ba1b4475f9b27981ebe0b
tlwman <miguel_ft@yahoo.com.ar>
2013-04-30 22:54:02 -0300

    se probo asi nomas y andaba q se yo...

--- practicas_ayed/src/p5e5/TestTOC.java
+++ practicas_ayed/src/p5e5/TestTOC.java
@@ -7,6 +7,28 @@ import java.util.Scanner;
 
 public class TestTOC {
 
+	public static ArbolGeneral<Contenido> gen(){
+		ArbolGeneral<Contenido> a = new ArbolGeneral<Contenido>(new Contenido("tapa", "java", 0, 'T'));
+		a.agregarHijo( new ArbolGeneral<Contenido>(new Contenido("capitulo1", "constructores", 1, 'C')) );
+		a.agregarHijo( new ArbolGeneral<Contenido>(new Contenido("capitulo3", "herencia", 12, 'C')) );
+		a.agregarHijo( new ArbolGeneral<Contenido>(new Contenido("capitulo3", "hilos", 21, 'C')) );
+		a.agregarHijo( new ArbolGeneral<Contenido>(new Contenido("apendice", "interfaces", 37, 'A')) );
+		
+		ArbolGeneral<Contenido> b = a.getHijos().elemento(0);
+		b.agregarHijo( new ArbolGeneral<Contenido>(new Contenido("1.1", "java", 1, 'S')) );
+		b.agregarHijo( new ArbolGeneral<Contenido>(new Contenido("1.2", "java", 4, 'S')) );
+		b.agregarHijo( new ArbolGeneral<Contenido>(new Contenido("1.3", "java", 8, 'S')) );
+
+		ArbolGeneral<Contenido> d = a.getHijos().elemento(2);
+		d.agregarHijo( new ArbolGeneral<Contenido>(new Contenido("3.1", "java", 21, 'S')) );
+		d.agregarHijo( new ArbolGeneral<Contenido>(new Contenido("3.2.1", "java", 29, 'S')) );
+
+		ArbolGeneral<Contenido> e = d.getHijos().elemento(1);
+		e.agregarHijo( new ArbolGeneral<Contenido>(new Contenido("3.2.2", "java", 32, 'S')) );
+		
+		return a;
+	}
+	
 	public static ArbolGeneral<Contenido> generar(){
 		ArbolGeneral<Contenido> a;
 		Scanner sc = new Scanner(System.in);
@@ -29,7 +51,15 @@ public class TestTOC {
 	
 	public static void main(String[] args){
 		
-		TablaDeContenidos toc = new TablaDeContenidos( TestTOC.generar() );
+		TablaDeContenidos toc = new TablaDeContenidos( TestTOC.gen() );
+		
+		ListaEnlazadaGenerica<String> n = toc.temasSecciones();
+		System.out.println("Capitulos: ");
+		n.comenzar();
+		while(!n.fin()){
+			System.out.println(n.elemento());
+			n.proximo();
+		}		
 		
 		ListaEnlazadaGenerica<Contenido> l = toc.capitulosMasAltos();
 		System.out.print("Capitulos con anidaciones mas profundas:");
