cc4b20b03e1952cf16bf418b6038c75674d4e58b
tlwman <miguel_ft@yahoo.com.ar>
2013-04-30 21:24:18 -0300

    primera version del ej. 5 completa

--- practicas_ayed/src/p5e5/Contenido.java
+++ practicas_ayed/src/p5e5/Contenido.java
@@ -5,11 +5,13 @@ public class Contenido {
 	private String nombre;
 	private String tema;
 	private int pagInicio;
+	private char tipo;
 	
-	public Contenido(String nombre, String tema, int pagInicio){
+	public Contenido(String nombre, String tema, int pagInicio, char tipo){
 		this.nombre = nombre;
 		this.tema = tema;
 		this.pagInicio = pagInicio;
+		this.tipo = tipo;
 	}
 	
 	public String getNombre() {
@@ -30,7 +32,19 @@ public class Contenido {
 	public void setPagInicio(int pagInicio) {
 		this.pagInicio = pagInicio;
 	}
-	
-	
-
+	public char getTipo() {
+		return tipo;
+	}
+	public void setTipo(char tipo) {
+		this.tipo = tipo;
+	}
+	public boolean esSeccion(){
+		return this.tipo == 'S';
+	}
+	public boolean esCapitulo(){
+		return this.tipo == 'C';
+	}
+	public boolean esApendice(){
+		return this.tipo == 'A';
+	}
 }
---
+++ practicas_ayed/src/p5e5/TablaDeContenidos.java
@@ -0,0 +1,102 @@
+package p5e5;
+
+import utiles.*;
+
+public class TablaDeContenidos {
+
+	private ArbolGeneral<Contenido> raiz;
+	
+	public TablaDeContenidos( ArbolGeneral<Contenido> toc){
+		this.raiz = toc;
+	}
+	
+	public ListaEnlazadaGenerica<String> temasSecciones(String capitulo){
+
+		ListaEnlazadaGenerica<String> retorno = new ListaEnlazadaGenerica<String>();
+		ListaEnlazadaGenerica<ArbolGeneral<Contenido>> hijos = this.raiz.getHijos();
+		String str = "";
+		
+		hijos.comenzar();
+		while (!hijos.fin()){
+			Contenido cont = hijos.elemento().getDatoRaiz();
+			
+			if ( cont.esCapitulo() ){
+				str = cont.getNombre() + ", " + cont.getTema() + ", "+ cont.getPagInicio()+", ";
+			}
+			else{
+				str = "";
+			}
+			hijos.proximo();
+			if ( !str.equals("") ){
+				str = str + (( !hijos.fin() )? (hijos.elemento().getDatoRaiz().getPagInicio() - 1) : "ultima página");
+				retorno.agregar(str);
+			}
+		}
+		return retorno;
+	}
+	
+	public ListaEnlazadaGenerica<Contenido> capitulosMasAltos(){
+		
+		ListaEnlazadaGenerica<ArbolGeneral<Contenido>> hijos = this.raiz.getHijos();
+		ListaEnlazadaGenerica<Contenido> retorno = new ListaEnlazadaGenerica<Contenido>();
+		
+		hijos.comenzar();
+		int maxAlt = 0;
+		while ( !hijos.fin() ){
+			
+			if (hijos.elemento().getDatoRaiz().esCapitulo()){
+				int altura = hijos.elemento().altura();
+				if ( altura > maxAlt ){
+					retorno = new ListaEnlazadaGenerica<Contenido>();
+					retorno.agregar(hijos.elemento().getDatoRaiz());
+					maxAlt = altura;
+				}
+				else if (maxAlt == altura){
+					retorno.agregar(hijos.elemento().getDatoRaiz());
+				}	
+			}
+			hijos.proximo();
+		}
+		return retorno;
+	}
+	
+	public static void buscarMasAnchos(ArbolGeneral<Contenido> a, ListaEnlazadaGenerica<Contenido> lista){
+		if (a.getDatoRaiz() != null){
+			int nHijos = a.getHijos().tamanio();
+			
+			if (lista.esVacia()){
+				lista.agregar(new Contenido("dummy", "cantHijos", nHijos, 'X'));
+				lista.agregar(a.getDatoRaiz());
+			}
+			else{
+				int max = lista.elemento(0).getPagInicio();
+				if (nHijos > max){
+					lista = new ListaEnlazadaGenerica<Contenido>();
+					lista.agregar(new Contenido("dummy", "cantHijos", nHijos, 'X'));
+					lista.agregar(a.getDatoRaiz());
+				}
+				else if (nHijos == max){
+					lista.agregar(a.getDatoRaiz());
+				}
+			}
+			
+			ListaEnlazadaGenerica<ArbolGeneral<Contenido>> hijos = a.getHijos();
+			
+			hijos.comenzar();
+			while (!hijos.fin()){
+				TablaDeContenidos.buscarMasAnchos(hijos.elemento(), lista);
+				hijos.proximo();
+			}
+		}
+	}
+	
+	public ListaEnlazadaGenerica<Contenido> capitulosMasAnchos(){
+			
+		ListaEnlazadaGenerica<Contenido> retorno = new ListaEnlazadaGenerica<Contenido>();
+		
+		TablaDeContenidos.buscarMasAnchos(this.raiz, retorno);
+		
+		return retorno;
+	}
+	
+}
